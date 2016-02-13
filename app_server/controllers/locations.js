02955ce5aec3abc5ed6e7c734449284d4e21b9f0
Apprentice-JT <yrq0712@gmail.com>
2016-02-13 13:50:15 -0500

    Chapter 7 404v2

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var apiOptions ={
 	server: "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production'){
-	apiOptions.server = "http://aqueous-falls-53717.herokuapp.com";
+	apiOptions.server = "https://aqueous-falls-53717.herokuapp.com";
 };
 
 /* GET 'home' page */
